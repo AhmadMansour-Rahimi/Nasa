da47d7a3041fab2daacd7cf002ecf96835deb28b
Mansour <mansourrahimi008@gmial.com>
2022-09-01 22:17:54 +0430

    mongodb is connected

--- app.js
+++ app.js
@@ -24,6 +24,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 
+//mongodbConnection
+var mongoose  = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL);
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
